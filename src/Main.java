5b16ba4a06575a4311a07afde1314cd92c7ac0cb
621352 <camarenburt>
2023-10-04 09:13:31 -0400

    gdfhy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Scanner(System.in);
+
+
+        System.out.println("What party are you affiliated with? D, R. I, or Other");
+        String party;
+
+        if (party == "D") {
+            System.out.println("Your a Democratic Donkey.");
+        }
+        if (party == "R") {
+            System.out.println("Your a Republican Elephant.");
+        }
+
+        if (party == "I") {
+            System.out.println("Your a Republican Elephant.");
+
+        }
+        if (party == "Other"){
+           System.out.println("Your another weird guy.");
+            }
+
     }
 }
+
